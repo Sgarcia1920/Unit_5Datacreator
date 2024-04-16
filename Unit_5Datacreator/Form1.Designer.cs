a046540727b7e08f5429e1c51bbee842db9043ab
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-04-15 21:21:27 -0600

    interface

--- Unit_5Datacreator/Form1.Designer.cs
+++ Unit_5Datacreator/Form1.Designer.cs
@@ -42,28 +42,32 @@
 			// 
 			// btncreate
 			// 
-			btncreate.Font = new Font("Tahoma", 12F);
-			btncreate.Location = new Point(155, 372);
+			btncreate.BackColor = Color.OliveDrab;
+			btncreate.FlatStyle = FlatStyle.Flat;
+			btncreate.Font = new Font("Tahoma", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			btncreate.ForeColor = Color.Ivory;
+			btncreate.Location = new Point(180, 372);
 			btncreate.Name = "btncreate";
 			btncreate.Size = new Size(113, 43);
 			btncreate.TabIndex = 0;
 			btncreate.Text = "Create";
-			btncreate.UseVisualStyleBackColor = true;
+			btncreate.UseVisualStyleBackColor = false;
 			btncreate.Click += btncreate_Click;
 			// 
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Font = new Font("Tahoma", 12F);
-			label1.Location = new Point(142, 51);
+			label1.Font = new Font("Humnst777 BlkCn BT", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			label1.Location = new Point(92, 35);
 			label1.Name = "label1";
-			label1.Size = new Size(174, 19);
+			label1.Size = new Size(291, 33);
 			label1.TabIndex = 1;
 			label1.Text = "Creates Data Randomly";
 			// 
 			// txtNames
 			// 
 			txtNames.Font = new Font("Tahoma", 12F);
+			txtNames.ForeColor = Color.Transparent;
 			txtNames.Location = new Point(155, 108);
 			txtNames.Name = "txtNames";
 			txtNames.ReadOnly = true;
@@ -73,26 +77,29 @@
 			// label2
 			// 
 			label2.AutoSize = true;
-			label2.Font = new Font("Tahoma", 12F);
-			label2.Location = new Point(92, 108);
+			label2.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic);
+			label2.ForeColor = Color.DarkBlue;
+			label2.Location = new Point(83, 108);
 			label2.Name = "label2";
-			label2.Size = new Size(57, 19);
+			label2.Size = new Size(64, 19);
 			label2.TabIndex = 3;
 			label2.Text = "Names";
 			// 
 			// label3
 			// 
 			label3.AutoSize = true;
-			label3.Font = new Font("Tahoma", 12F);
-			label3.Location = new Point(77, 168);
+			label3.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic);
+			label3.ForeColor = Color.DarkBlue;
+			label3.Location = new Point(68, 168);
 			label3.Name = "label3";
-			label3.Size = new Size(72, 38);
+			label3.Size = new Size(84, 38);
 			label3.TabIndex = 5;
 			label3.Text = "Paternal \r\nSurname";
 			// 
 			// txtpaternalsurname
 			// 
 			txtpaternalsurname.Font = new Font("Tahoma", 12F);
+			txtpaternalsurname.ForeColor = Color.Transparent;
 			txtpaternalsurname.Location = new Point(155, 179);
 			txtpaternalsurname.Name = "txtpaternalsurname";
 			txtpaternalsurname.ReadOnly = true;
@@ -102,16 +109,18 @@
 			// label4
 			// 
 			label4.AutoSize = true;
-			label4.Font = new Font("Tahoma", 12F);
-			label4.Location = new Point(77, 243);
+			label4.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic);
+			label4.ForeColor = Color.DarkBlue;
+			label4.Location = new Point(68, 243);
 			label4.Name = "label4";
-			label4.Size = new Size(72, 38);
+			label4.Size = new Size(81, 38);
 			label4.TabIndex = 7;
 			label4.Text = "Mother \r\nSurname";
 			// 
 			// txtmothersurname
 			// 
 			txtmothersurname.Font = new Font("Tahoma", 12F);
+			txtmothersurname.ForeColor = Color.Transparent;
 			txtmothersurname.Location = new Point(155, 254);
 			txtmothersurname.Name = "txtmothersurname";
 			txtmothersurname.ReadOnly = true;
@@ -121,16 +130,18 @@
 			// label5
 			// 
 			label5.AutoSize = true;
-			label5.Font = new Font("Tahoma", 12F);
-			label5.Location = new Point(77, 303);
+			label5.Font = new Font("Tahoma", 12F, FontStyle.Bold | FontStyle.Italic);
+			label5.ForeColor = Color.DarkBlue;
+			label5.Location = new Point(68, 303);
 			label5.Name = "label5";
-			label5.Size = new Size(60, 38);
+			label5.Size = new Size(69, 38);
 			label5.TabIndex = 9;
 			label5.Text = "Date of\r\nBirth";
 			// 
 			// txtdateofbirth
 			// 
 			txtdateofbirth.Font = new Font("Tahoma", 12F);
+			txtdateofbirth.ForeColor = Color.Transparent;
 			txtdateofbirth.Location = new Point(155, 314);
 			txtdateofbirth.Name = "txtdateofbirth";
 			txtdateofbirth.ReadOnly = true;
@@ -141,6 +152,7 @@
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.GradientActiveCaption;
 			ClientSize = new Size(498, 450);
 			Controls.Add(label5);
 			Controls.Add(txtdateofbirth);
@@ -153,6 +165,7 @@
 			Controls.Add(label1);
 			Controls.Add(btncreate);
 			Name = "Form1";
+			StartPosition = FormStartPosition.CenterScreen;
 			Text = "Form1";
 			ResumeLayout(false);
 			PerformLayout();
